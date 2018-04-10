a4b3ee68efc2fbd14aaf191288ab1e2bb100aec3
Ben Wiedermann <benw@cs.hmc.edu>
2018-04-10 00:48:01 -0700

    step 3

--- src/main/scala/pioneer/LibraryEmpty.scala
+++ src/main/scala/pioneer/LibraryEmpty.scala
@@ -8,11 +8,58 @@ object LibraryEmpty extends PicobotGUIApp {
 
   rules =
     List(
-      Rule(
-        State(0),
-        Surroundings(Anything, Anything, Anything, Anything),
-        StayHere,
-        State(0)
+      Rule(State("0"),
+        Surroundings(Anything, Anything, Open, Anything),
+        West,
+        State("0")
+      ),
+
+      Rule(State("0"),
+        Surroundings(Open, Anything, Blocked, Anything),
+        North,
+        State("1")
+      ),
+
+      Rule(State("0"),
+        Surroundings(Blocked, Open, Blocked, Anything),
+        South,
+        State("2")
+      ),
+
+      Rule(State("1"),
+        Surroundings(Open, Anything, Anything, Anything),
+        North,
+        State("1")
+      ),
+
+      Rule(State("1"),
+        Surroundings(Blocked, Anything, Anything, Open),
+        South,
+        State("2")
+      ),
+
+      Rule(State("2"),
+        Surroundings(Anything, Anything, Anything, Open),
+        South,
+        State("2")
+      ),
+
+      Rule(State("2"),
+        Surroundings(Anything, Open, Anything, Blocked),
+        East,
+        State("3")
+      ),
+
+      Rule(State("3"),
+        Surroundings(Open, Anything, Anything, Anything),
+        North,
+        State("3")
+      ),
+
+      Rule(State("3"),
+        Surroundings(Blocked, Open, Anything, Anything),
+        East,
+        State("2")
       )
     )
 
