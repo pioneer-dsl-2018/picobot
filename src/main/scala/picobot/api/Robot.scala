653169ba32aaf519922e31d7543d29a9a92b9b3c
Teo <zengwc.teo2016@outlook.com>
2018-04-14 09:11:31 +0800

    add empty functions

--- src/main/scala/picobot/api/Robot.scala
+++ src/main/scala/picobot/api/Robot.scala
@@ -1,7 +1,12 @@
 package picobot.api
 
-import picolib.Rule
+import picolib.{MoveDirection, Rule}
 
 class Robot(var rules: List[Rule] = List()) {
+  def sweep(pos:MoveDirection): Robot{}
+
+  def step(pos:MoveDirection): Robot{}
+
+  def goto(state: Int): Robot {}
 
 }
