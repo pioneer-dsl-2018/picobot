724cc532fc7a6d46a4ab06d1b135e029a55f85ee
Teo <zengwc.teo2016@outlook.com>
2018-04-14 10:00:03 +0800

    New Sweep Method!

--- src/main/scala/picobot/api/Robot.scala
+++ src/main/scala/picobot/api/Robot.scala
@@ -1,12 +1,51 @@
 package picobot.api
 
-import picolib.{MoveDirection, Rule}
+import picolib._
 
 class Robot(var rules: List[Rule] = List()) {
-  def sweep(pos:MoveDirection): Robot{}
+  private var stateNow: Int = 0
 
-  def step(pos:MoveDirection): Robot{}
+  //Hope this works
+  def identifyOpenSurroundings(FutureDirection:MoveDirection):Surroundings={
+    if(FutureDirection==North)
+      Surroundings(Open,Anything, Anything, Anything)
+    if(FutureDirection==East)
+      Surroundings(Anything, Open, Anything, Anything)
+    if(FutureDirection==West)
+      Surroundings(Anything,Anything, Open, Anything)
+    else
+      Surroundings(Anything,Anything, Anything, Open)
+  }
 
-  def goto(state: Int): Robot {}
+  def identifyBlockedSurroundings(FutureDirection:MoveDirection):Surroundings={
+    if(FutureDirection==North)
+      Surroundings(Blocked,Anything, Anything, Anything)
+    if(FutureDirection==East)
+      Surroundings(Anything, Blocked, Anything, Anything)
+    if(FutureDirection==West)
+      Surroundings(Anything,Anything, Blocked, Anything)
+    else
+      Surroundings(Anything,Anything, Anything, Blocked)
+  }
+
+  def sweep(FutureDirection:MoveDirection): Robot={
+
+    val Newrule = Rule(
+      State(this.stateNow),
+      identifyOpenSurroundings(FutureDirection),
+      FutureDirection,
+      State(this.stateNow+1))
+    this.rules = this.rules :+ Newrule
+
+    this
+  }
+
+  def step(pos:MoveDirection): Robot={
+
+  }
+
+  def goto(state: Int): Robot ={
+
+  }
 
 }
