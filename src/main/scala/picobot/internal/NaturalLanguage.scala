77e0c5c7d676e81153b4d47f15f7bb94b5d825fe
Teo <zengwc.teo2016@outlook.com>
2018-04-16 19:16:36 +0800

    added blank implementation

--- src/main/scala/picobot/internal/NaturalLanguage.scala
+++ src/main/scala/picobot/internal/NaturalLanguage.scala
@@ -1,9 +1,23 @@
 package picobot.internal
 
 import picobot.api.Robot
+import picolib._
 
 trait NaturalLanguage {
   val robot: Robot
 
-  // fill in your implementation here
+  implicit class Rule(val StateNow: Int){
+    def sweep (FutureDirection: MoveDirection): Robot={
+
+    }
+
+    def goto (FutureState: Int): Robot = {
+
+    }
+
+    def step (FutureDirection: MoveDirection): Robot = {
+
+    }
+  }
+
 }
