2731261b838e004d66842c1b09241b67fc992a00
Eesha Agarwal <eesha2301@gmail.com>
2018-04-18 13:21:09 +0530

    Empty Room Picobot
    Empty room program for Picobot Library

--- src/main/scala/pioneer/LibraryEmpty.scala
+++ src/main/scala/pioneer/LibraryEmpty.scala
@@ -1,6 +1,6 @@
 package pioneer
 
-import picolib._
+import picolib.{Rule, _}
 import picolib.displays._
 
 object LibraryEmpty extends PicobotGUIApp {
@@ -10,10 +10,64 @@ object LibraryEmpty extends PicobotGUIApp {
     List(
       Rule(
         State(0),
-        Surroundings(Anything, Anything, Anything, Anything),
-        StayHere,
+        Surroundings(Anything, Open, Anything, Anything),
+        East,
         State(0)
-      )
+      ),
+      Rule(
+        State(0),
+        Surroundings(Anything, Blocked, Anything, Open),
+        South,
+        State(1)
+      ),
+      Rule(
+        State(0),
+        Surroundings(Open, Blocked, Anything, Blocked),
+        StayHere,
+        State(2)
+      ),
+      Rule(
+        State(1),
+        Surroundings(Anything, Anything, Open, Open),
+        West,
+        State(1)
+      ),
+      Rule(
+        State(1),
+        Surroundings(Anything, Anything, Blocked, Anything),
+        South,
+        State(0)
+      ),
+      Rule(
+        State(1),
+        Surroundings(Open, Blocked, Open, Blocked),
+        StayHere,
+        State(2)
+      ),
+      Rule(
+        State(2),
+        Surroundings(Open, Anything, Anything, Anything),
+        North,
+        State(2)
+      ),
+      Rule(
+        State(2),
+        Surroundings(Blocked, Anything, Anything, Anything),
+        West,
+        State(3)
+      ),
+      Rule(
+        State(3),
+        Surroundings(Anything, Anything, Anything, Open),
+        South,
+        State(3)
+      ),
+      Rule(
+        State(3),
+        Surroundings(Anything, Anything, Anything, Blocked),
+        West,
+        State(2)
+      ),
     )
 
   run()
