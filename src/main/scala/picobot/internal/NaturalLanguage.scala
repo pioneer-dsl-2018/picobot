68550e6774c996ff89c04a5ce903534480c1149a
Yufan(Harry) Liu <harrylyf@gmail.com>
2018-04-22 19:19:43 -0400

    Remove the host flavor, to create an internal DSL

--- src/main/scala/picobot/internal/NaturalLanguage.scala
+++ src/main/scala/picobot/internal/NaturalLanguage.scala
@@ -1,9 +1,30 @@
 package picobot.internal
 
 import picobot.api.Robot
+import picolib._
+import picolib.MoveDirection
 
 trait NaturalLanguage {
   val robot: Robot
 
-  // fill in your implementation here
+  implicit class State(statenumber: Int) {
+
+    def step(direction:MoveDirection): Robot = {
+      robot.currentstate = statenumber
+      robot.step(direction)
+    }
+
+    def sweep(direction:MoveDirection): Robot = {
+      robot.currentstate = statenumber
+      robot.sweep(direction)
+    }
+
+    def goto(state:Int): Robot = {
+      robot.currentstate = statenumber
+      robot.goto(state)
+    }
+
+  }
+
+
 }
