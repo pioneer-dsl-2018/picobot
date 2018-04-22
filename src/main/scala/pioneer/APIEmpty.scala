79cec27cb73e741a10fa0bc5e3e2e4d840ef45c7
Yufan(Harry) Liu <harrylyf@gmail.com>
2018-04-22 18:38:33 -0400

    no idea what is this; came out systematically; just ignore

---
+++ src/main/scala/pioneer/APIEmpty.scala
@@ -0,0 +1,24 @@
+package pioneer
+
+import picolib._
+import picolib.displays._
+
+import picobot.api.Robot
+
+object APIEmpty extends PicobotGUIApp {
+  map = Map(resource("/empty.txt"))
+
+  val robot = new Robot
+
+  robot.sweep(West)  // state 0
+    .sweep(North) // state 1
+    .sweep(South) // state 2
+    .step(East)   // state 3
+    .sweep(North) // state 4
+    .step(East)   // state 5
+    .goto(2)      // state 6
+
+  rules = robot.rules
+
+  run()
+}
